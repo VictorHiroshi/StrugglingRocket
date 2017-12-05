e865666aae4edbf724a2e2cedf77b4807fca83d6
VictorHiroshi <victor_hiroshi@hotmail.com>
2017-12-05 20:30:53 -0200

    Prototyping Rocket movement

---
+++ Struggling Rocket/Assets/Scripts/RocketController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour {
+
+	public float forwardSpeed = 1f;
+	public float rotationSpeed = 30f;
+
+	private Rigidbody2D mRigidBody;
+	private Vector2 speed;
+
+	void Awake()
+	{
+		mRigidBody = GetComponent <Rigidbody2D> ();
+
+		speed = new Vector2 (0f, forwardSpeed);
+
+		mRigidBody.velocity = speed;
+	}
+
+	void Update()
+	{
+		mRigidBody.MoveRotation (-1f * rotationSpeed * Input.GetAxis ("Horizontal"));
+
+		mRigidBody.AddForce (transform.up * forwardSpeed);
+	}
+}
