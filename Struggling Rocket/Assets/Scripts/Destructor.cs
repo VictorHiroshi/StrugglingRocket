1ec0d47b257dc27264b4dee527b93b801c39e28f
VictorHiroshi <victor_hiroshi@hotmail.com>
2017-12-06 09:54:24 -0200

    Polishments

---
+++ Struggling Rocket/Assets/Scripts/Destructor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructor : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		GameObject other = coll.gameObject;
+		Debug.Log (other.tag);
+		if(other.tag == "Planet")
+		{
+			Destroy (other);
+		}
+	}
+}
