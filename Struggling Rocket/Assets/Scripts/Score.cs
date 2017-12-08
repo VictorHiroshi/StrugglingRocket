bb8a7c84640ccbc0b1a3c747058c65a108820ae6
VictorHiroshi <victor_hiroshi@hotmail.com>
2017-12-08 14:57:32 -0200

    Measure distance traveled

---
+++ Struggling Rocket/Assets/Scripts/Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public Text scoreText;
+	public Transform player;
+
+	private float distance;
+
+	void Start()
+	{
+		distance = 0f;
+	}
+
+	void LateUpdate()
+	{
+		if (player.position.y > distance)
+			distance = player.position.y;
+
+		scoreText.text = "Distance: " + distance.ToString ("F2") + "km";
+	}
+}
