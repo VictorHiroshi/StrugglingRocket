79748cf283f6436bd2afb43f5402e1ace6a5a1c2
VictorHiroshi <victor_hiroshi@hotmail.com>
2018-01-01 21:15:07 -0200

    Change music and closes the app

---
+++ Struggling Rocket/Assets/Scripts/CloseScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CloseScene : MonoBehaviour {
+	
+	void LateUpdate () {
+		if(Input.GetKeyUp (KeyCode.Escape))
+		{
+			if (SceneManager.GetActiveScene ().buildIndex != 0)
+				SceneManager.LoadScene (0);
+			else
+			{
+				Application.Quit ();
+			}
+		}
+	}
+}
